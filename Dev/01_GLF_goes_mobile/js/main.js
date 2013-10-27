fe22b9766edca2bd1b56a203158974b1f70cd731
SebastianPetric <sebastian.petoecz@hs-furtwangen.de>
2013-10-27 17:57:32 +0100

    updated Branch

--- Dev/01_GLF_goes_mobile/js/main.js
+++ Dev/01_GLF_goes_mobile/js/main.js
@@ -1,64 +1,35 @@
+/*Define global vars */
+var activeItem;
+var navMain = $('#navMain');
+var navAccordion = $('#navAccordion');
+var expandItem;
+var expandTimeout;
+
 $(document).ready(function() {
-
+	/*************Video Player Functions *********/
+	var contentWdth = $('.content').width();
+	$('video').css('width', contentWdth+'px');
+	console.log($('.projekktor').width());
+	projekktor('video', {
+		volume : 0.8,
+		controls : true,
+		autoplay : false,
+		thereCanBeOnlyOne : true, //stop all other player instances but the one the user clicked play
+		ratio : 16 / 9,
+		leaveFullscreen : true, //player will try to leave fullscreen once the "done" event has been fired
+		disallowSkip : false,
+		showOnStart : true,
+		addplugins : ['controlbar'],
+		playerFlashMP4 : 'http://www.glftv.de:8080/video-player/jarisplayer.swf',
+		playerFlashMP3 : 'http://www.glftv.de:8080/video-player/jarisplayer.swf'
+	});
+	/*************Video Player Functions End*********/
 	/************ Nav Functions *************/
-	/*Define global vars */
-	var activeItem;
-	var navMain = $('#navMain');
-	var expandItem;
-	var expandTimeout;
-
 	/* Call accordionToggle function if .collapsible is hovered for 300 millisecs */
 	$(navMain).find('ul > .collapsible').hover(function() {
 		expandItem = $(this);
 		expandTimeout = setTimeout(accordionToggle, 300);
 	}, stopAnimation);
-
-	/* expand hovered Item and collapse former activeItem */
-	function accordionToggle() {
-		activeItem = $(navMain).find('ul > .active');
-		if ($(activeItem).hasClass('search-btn')) {
-			$(activeItem).animate({
-				width : "73px"
-			}, {
-				duration : 300,
-				queue : false
-			});
-		} else {
-			$(activeItem).animate({
-				width : "126px"
-			}, {
-				duration : 300,
-				queue : false
-			});
-		}
-		if ($(expandItem).hasClass('search-btn')) {
-			$(expandItem).animate({
-				width : "326px"
-			}, {
-				duration : 300,
-				queue : false
-			});
-		} else {
-			$(expandItem).animate({
-				width : "379px"
-			}, {
-				duration : 300,
-				queue : false
-			});
-		}
-		$(activeItem).removeClass('active');
-		activeItem = expandItem;
-		$(activeItem).addClass('active');
-		/*set focus on search input if search is active element*/
-		if ($(activeItem).hasClass('search-btn')) {
-			$(activeItem).find('#searchInput').focus();
-		}
-	}
-
-	function stopAnimation() {
-		clearTimeout(expandTimeout);
-	}
-
 	/* Collapse active Item with some timeout on mouseleave*/
 	$(navMain).mouseleave(function() {
 		$('.nav-btn-wrapper').each(function(index, elem) {
@@ -97,25 +68,9 @@ $(document).ready(function() {
 		$(this).click(function() {
 			$(overlayForm).toggleClass('visible').toggleClass('hidden');
 			$(overlaySubmitContent).toggleClass('visible').toggleClass('hidden');
-
 		});
 	});
 	/************** Only for Testing Functions End*****************/
-	/*************Video Player Functions *********/
-	projekktor('.video-clip', {
-		volume : 0.8,
-		controls : true,
-		autoplay : false,
-		thereCanBeOnlyOne : true, //stop all other player instances but the one the user clicked play
-		ratio : 16 / 9,
-		leaveFullscreen : true, //player will try to leave fullscreen once the "done" event has been fired
-		disallowSkip : false,
-		showOnStart : true,
-		addplugins : ['controlbar'],
-		playerFlashMP4 : 'http://www.glftv.de:8080/video-player/jarisplayer.swf',
-		playerFlashMP3 : 'http://www.glftv.de:8080/video-player/jarisplayer.swf'
-	});
-	/*************Video Player Functions End*********/
 	/*************Detail Pages Info-Wrapper Functions*********/
 	var infoWrapper = $('.info-wrapper');
 	var infoContent = $(infoWrapper).find('.info-content');
@@ -125,6 +80,7 @@ $(document).ready(function() {
 		infoWrapper.css('height', '407px');
 	}
 	/*************Detail Pages Info-Wrapper Functions End*********/
+	/*************Topic Page Functions *****************/
 	/*************Overlay Functions *****************/
 	var addTopicTile = $("#add_topic_tile");
 	var addTopicOverlay = $('#add_topic_overlay');
@@ -132,6 +88,40 @@ $(document).ready(function() {
 		overlayToggle(addTopicOverlay);
 	});
 	/*************Overlay Functions End*****************/
+	/*************Topic Page Functions End*****************/
+	/*************Live Page Functions*****************/
+	/*************Overlay Functions *****************/
+	var pageLive = $('#page_live');
+	var showClipTeaserTiles = $(pageLive).find(".rel-teaser-tile");
+	//Toggle Overlay depending on id of clicked tile and open the matching overlay by comparing the IDs
+	$(showClipTeaserTiles).each(function() {
+		$(this).click(function() {
+			var topicId = $(this).attr('id');
+			var clipTeaserOverlay = $('#' + topicId + '_overlay');
+			overlayToggle(clipTeaserOverlay);
+		});
+	});
+	/*************Overlay Functions End*****************/
+	/*************Live Page Functions End*****************/
+	/*************Jobs Page Functions*****************/
+	/*************Overlay Functions *****************/
+	var showInfoTile = $("#show_jobs_info");
+	var jobsInfoOverlay = $('#jobs_info_overlay');
+	$(showInfoTile).click(function() {
+		overlayToggle(jobsInfoOverlay);
+	});
+	var jobTiles = $('.job-teaser-tile');
+	//Toggle Overlay depending on id of clicked tile and open the matching overlay by comparing the IDs
+	$(jobTiles).each(function() {
+		$(this).click(function() {
+			var jobId = $(this).attr('id');
+			var jobApplyOverlay = $('#' + jobId + '_overlay');
+			console.log(jobApplyOverlay);
+			overlayToggle(jobApplyOverlay);
+		});
+	});
+	/*************Overlay Functions End*****************/
+	/*************Jobs Page Functions End*****************/
 	/*************Search Page Functions *****************/
 	var filterBtn = [];
 	filterBtn.push($('#filter_clips_btn'));
@@ -150,28 +140,106 @@ $(document).ready(function() {
 
 /**************** WINDOW RESIZE *********************/
 $(window).resize(function() {
+	/*resize video player*/
+	var contentWdth = $('.content').width();
+	$('.projekktor').css('width', contentWdth+'px');
 	/*call functions */
 	sliderSize();
 	resizeInfoWrapper();
-});
+	});
 /***********************WINDOW RESIZE END ********************/
+/*************Nav Accordion Toggle Functions *****************/
+/* expand hovered Item and collapse former activeItem */
+function accordionToggle() {
+	activeItem = $(navMain).find('ul > .active');
+	if ($(activeItem).hasClass('search-btn')) {
+		var searchResLayer = $('#search_results_layer');
+		$(searchResLayer).removeClass('has-results').addClass('no-results');
+		$(navAccordion).css('overflow', 'hidden');
+		$(activeItem).animate({
+			width : "73px"
+		}, {
+			duration : 300,
+			queue : false
+		});
+	} else {
+		$(activeItem).animate({
+			width : "126px"
+		}, {
+			duration : 300,
+			queue : false
+		});
+	}
+	if ($(expandItem).hasClass('search-btn')) {
+		$(expandItem).animate({
+			width : "326px"
+		}, {
+			duration : 300,
+			queue : false
+		});
+	} else {
+		$(expandItem).animate({
+			width : "379px"
+		}, {
+			duration : 300,
+			queue : false
+		});
+	}
+	$(activeItem).removeClass('active');
+	activeItem = expandItem;
+	$(activeItem).addClass('active');
+	/*set focus on search input if search is active element*/
+	if ($(activeItem).hasClass('search-btn')) {
+		var searchInput = $(activeItem).find('#search_input');
+		$(searchInput).focus();
+		searchLayerToggle(searchInput);
+	}
+}
+
+function stopAnimation() {
+	clearTimeout(expandTimeout);
+}
+
+/*************Nav Accordion Toggle Functions End *****************/
+/*************Search Layer Functions *****************/
+function searchLayerToggle(searchInput) {
+	var searchResLayer = $('#search_results_layer');
+	$(searchInput).bind("change paste keyup", function() {
+		if ($(searchInput).val() == '') {
+			console.log('no val');
+			if ($(searchResLayer).hasClass('has-results')) {
+				$(searchResLayer).removeClass('has-results').addClass('no-results');
+				$(navAccordion).css('overflow', 'hidden');
+			}
+		} else {
+			console.log('val');
+			if ($(searchResLayer).hasClass('no-results')) {
+				$(searchResLayer).removeClass('no-results').addClass('has-results');
+				$(navAccordion).css('overflow', 'visible');
+			}
+		}
+	});
+}
+
+/*************Search Layer Functions End *****************/
 /*************Search Page Functions *****************/
 function toggleSearchFilters(filterBtn) {
 	var filterType = $(filterBtn).attr('id');
 	filterType = filterType.split('_');
-	console.log(filterType);
 }
 
 /*************Search Page Functions End*****************/
 /***********************overlay functions********************/
 function overlayToggle(overlay) {
+	var closeOverlay = [];
 	var overlayContent = overlay.find('.overlay-content');
 	var overlayCloseIcon = overlayContent.find('.overlay-close-btn');
-	var overlayCloseBtn = overlayContent.find('.close-overlay-btn');
-	var closeOverlay = [];
+	if (overlayContent.find('.close-overlay-btn')) {
+		var overlayCloseBtn = overlayContent.find('.close-overlay-btn');
+		closeOverlay.push($(overlayCloseBtn));
+	}
 	closeOverlay.push($(overlay));
 	closeOverlay.push($(overlayCloseIcon));
-	closeOverlay.push($(overlayCloseBtn));
 	$(overlay).fadeIn(300);
 	$(overlay).addClass('active');
 	$('body').css('overflow', 'hidden');
