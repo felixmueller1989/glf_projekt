5bb4d8521cc2834794ce1daa203a05faf9c82f4d
Ramona Rapp <rappramona@gmx.de>
2013-11-14 13:18:15 +0100

    Added Backup to fix wrong commits

--- Dev/01_GLF_goes_mobile/js/main.js
+++ Dev/01_GLF_goes_mobile/js/main.js
@@ -14,10 +14,6 @@ var tabsInitialized = false;
 var filterInitialized = false;
 var filterHeight;
 var tileHeightChanged = false;
-var label_left = $(document.createElement('span'));
-var label_right= $(document.createElement('span'));
-var semRangeValue = new Array();
-var semRangeText =['WS1','SoSe1','WS2','SoSe2','WS3','SoSe3','WS4','SoSe4','WS5','SoSe5','WS6','SoSe6'];
 
 $(window).load(function() {
 	$('.flexslider').flexslider({
@@ -27,22 +23,6 @@ $(window).load(function() {
 		prevText : " ",
 		nextText : " ",
 	});
-
-/*************Init Filter Range Slider Functions *********/
-$('#filter-range-slider').noUiSlider({
-		start: [5, 10]
-		,range: [0, 11]
-		,step: 1
-		,connect: true
-		,handles: 2
-		,slide: function(){
-			semRangeValue = $(this).val();
-			createLabels(semRangeValue);
-		}
-	});
-	filterRangeInit();
-/*************Init Filter Range Slider End*********/
-
 });
 
 $(document).ready(function() {
@@ -63,6 +43,13 @@ $(document).ready(function() {
 		addplugins : ['controlbar'],
 		playerFlashMP4 : 'http://www.glftv.de:8080/video-player/jarisplayer.swf',
 		playerFlashMP3 : 'http://www.glftv.de:8080/video-player/jarisplayer.swf'
+	});
+	// Initialise noUiSlider
+	$("#filter-range-slider").noUiSlider({
+		start : [20, 80],
+		range : [0, 100],
+		connect : true,
+		handles : 2
 	});
 	/*************Video Player Functions End*********/
 
@@ -85,6 +72,10 @@ $(document).ready(function() {
 		$(this).click(function() {
 			toggleSearchFilters(this);
 		});
+	});
+
+	$('.call-modal').click(function() {
+		console.log('click');
 	});
 	/*************Search Page Functions End*****************/
 	/*call functions */
@@ -432,40 +423,3 @@ function testing() {
 }
 
 /************** Only for Testing Functions End*****************/
-/************* Filter Range Slider Functions *********/
-function createLabels(semRangeValue){
-        
-        $(label_left).appendTo('#filter-range-slider');
-        $(label_right).appendTo('#filter-range-slider');
-
-        for (var i=0; i <= semRangeText.length;i++){
-                if(semRangeValue[0] == i){
-                        $(label_left).text(semRangeText[i]);
-                }
-                if(semRangeValue[1] == i){
-                        $(label_right).text(semRangeText[i]);
-                }
-        }
-}
-function filterRangeInit(){
-        semRangeValue = $('#filter-range-slider').val();
-        
-        $(label_left).addClass('noUi-handle-lower-label');
-        $(label_right).addClass('noUi-handle-upper-label');
-
-        $(label_left).addClass('noUi-handle-label');
-        $(label_right).addClass('noUi-handle-label');
-
-        $(label_left).appendTo('#filter-range-slider');
-        $(label_right).appendTo('#filter-range-slider');
-
-        for (var i=0; i <= semRangeText.length;i++){
-                if(semRangeValue[0] == i){
-                        $(label_left).text(semRangeText[i]);
-                }
-                if(semRangeValue[1] == i){
-                        $(label_right).text(semRangeText[i]);
-                }
-        }
-}
-/************* Filter Range Slider End*********/
